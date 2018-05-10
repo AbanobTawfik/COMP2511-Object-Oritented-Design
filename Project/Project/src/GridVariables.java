40c900431811d40fa83c7701b2c04799cf30f08a
Abanob Tawfik <z5075490@ad.unsw.edu.au>
2018-05-10 11:31:06 +1000

    fucking cancer omfg im so angry

--- Project/Project/src/GridVariables.java
+++
@@ -1,9 +0,0 @@
-public class GridVariables {
-    public static final Grid grid = new Grid();
-    public static final int boardSize = grid.getBoardSize();
-    public static final double gridWidth = 1440;
-    public static final double gridHeight = 900;
-    public static final double tileSizeWidth = gridWidth / boardSize;
-    public static final double tileSizeHeight = gridHeight / boardSize;
-
-}
--- Project/src/GridConstants.java
+++
@@ -1,7 +0,0 @@
-public enum GridConstants {
-    int boardSize = 6;
-    double gridWidth = 1440;
-    double gridHeight = 900;
-    double tileSizeWidth = gridWidth/boardSize;
-    double tileSizeHeight = gridHeight/boardSize;
-}
