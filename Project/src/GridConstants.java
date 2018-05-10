9b782bf819ba8d38ef3b41f92305530c821a7088
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-05-10 11:33:41 +1000

    Delete GridConstants.java

--- Project/src/GridConstants.java
+++
@@ -1,7 +0,0 @@
-public class GridConstants {
-    int boardSize = 6;
-    double gridWidth = 1440;
-    double gridHeight = 900;
-    double tileSizeWidth = gridWidth/boardSize;
-    double tileSizeHeight = gridHeight/boardSize;
-}
