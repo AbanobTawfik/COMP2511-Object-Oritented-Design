657989918c651878bbb1ad648ac1f4ce7d52d0e8
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-05-10 11:32:14 +1000

    :b:

---
+++ Project/src/GridConstants.java
@@ -0,0 +1,7 @@
+public class GridConstants {
+    int boardSize = 6;
+    double gridWidth = 1440;
+    double gridHeight = 900;
+    double tileSizeWidth = gridWidth/boardSize;
+    double tileSizeHeight = gridHeight/boardSize;
+}
